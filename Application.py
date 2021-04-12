9be6fd0fc555fb609f5d11f3dc7c8a533d7fa910
jonrrich <15861575+jonrrich@users.noreply.github.com>
2021-04-12 16:06:27 -0400

    resizing images

--- Application.py
+++ Application.py
@@ -50,7 +50,7 @@ def select_regions(img,region_type):
 
 
 def main():
-    Img = Image('images/dog.jpg')
+    Img = Image('images/dog3.jpg')
     img = Img.img
 
     obj_regions = select_regions(img,"object")
@@ -65,10 +65,10 @@ def main():
     G = GraphAlgos.G
     print("Graph made")
 
-    #G.show()
+    G.show()
     G.min_cut()
     print("Min cut found")
-    #G.show()
+    G.show()
 
     segmented = Img.segmentation(G.partition_S_labels)
     plt.imshow(segmented)
