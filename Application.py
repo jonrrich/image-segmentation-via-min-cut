0dbfce7872921820a8eced52973a5459df001bdf
bretsilverstein <56207228+bretsilverstein@users.noreply.github.com>
2021-04-14 00:43:57 -0400

    Fixed img weights!

--- Application.py
+++ Application.py
@@ -79,13 +79,13 @@ def test_lambda():
     background_regions = select_regions(img,"background")
     plt.close()
 
-    obj_seeds = set([(x,y) for reg in obj_regions for y in range(reg[0],reg[1]+1) for x in range(reg[2],reg[3]+1)])
-    back_seeds = set([(x,y) for reg in background_regions for y in range(reg[0],reg[1]+1) for x in range(reg[2],reg[3]+1)])
+    obj_seeds = set([(x,y,0) for reg in obj_regions for y in range(reg[0],reg[1]+1) for x in range(reg[2],reg[3]+1)])
+    back_seeds = set([(x,y,0) for reg in background_regions for y in range(reg[0],reg[1]+1) for x in range(reg[2],reg[3]+1)])
     print("Seeds created")
 
-    lm_list = np.array([i for i in range(0,600,100)])/10
+    lm_list = np.array([i for i in range(0,500,50)])/10
     for lm in lm_list:
-        GraphAlgos = GraphAlgorithms([Img.gray_img], back_seeds, obj_seeds, lmbda=lm)
+        GraphAlgos = GraphAlgorithms(np.array([Img.gray_img]), back_seeds, obj_seeds, lmbda=lm)
         G = GraphAlgos.G
         print("Graph made")
         #G.show()
@@ -94,8 +94,9 @@ def test_lambda():
         #G.show()
         plt.close()
 
-        segmented = Img.segmentation(G.partition_S_labels)
-        plt.imshow(segmented)
+        segmented = Img.segmentation(G.partition_S_labels,process=True)
+        masked = Img.apply_mask(segmented)
+        plt.imshow(masked)
         plt.title("Lambda = " + str(lm), fontsize=16)
         plt.draw()
 
@@ -115,7 +116,7 @@ def run_img():
 
     print("Seeds created")
 
-    GraphAlgos = GraphAlgorithms(np.array([Img.gray_img]), back_seeds, obj_seeds)
+    GraphAlgos = GraphAlgorithms(np.array([Img.gray_img]), back_seeds, obj_seeds,lmbda=0.1)
     G = GraphAlgos.G
     print("Graph made")
     #G.show()
--- GraphAlgorithms.py
+++ GraphAlgorithms.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 
 class GraphAlgorithms:
 
-    def __init__(self, frames, tSeeds, sSeeds, lmbda=10, R_bins=5):
+    def __init__(self, frames, tSeeds, sSeeds, lmbda=0.1, R_bins=10):
 
         self.lmbda = lmbda
 
@@ -38,10 +38,9 @@ class GraphAlgorithms:
 
     def construct_B_vid(self):
 
-
-        axis0 = np.gradient(np.gradient(frames,axis=0),axis=0)
-        axis1 = np.gradient(np.gradient(frames,axis=1),axis=1)
-        axis2 = np.gradient(np.gradient(frames,axis=2),axis=2)
+        axis0 = np.gradient(np.gradient(self.frames,axis=0),axis=0)
+        axis1 = np.gradient(np.gradient(self.frames,axis=1),axis=1)
+        axis2 = np.gradient(np.gradient(self.frames,axis=2),axis=2)
 
         for z in range(self.frames.shape[0]):
             for x in range(self.frames.shape[2]):
@@ -82,12 +81,20 @@ class GraphAlgorithms:
                 if y+1 < frames.shape[0]:
                     edge = [(x, y,0), (x, y+1,0)]
                     laplacian_edge = (abs(axis0[edge[0][1], edge[0][0]]) + abs(axis0[edge[1][1], edge[1][0]]))/2
-                    self.B[frozenset(edge)] = -1 if laplacian_edge==0 else 1/laplacian_edge
+
+                    if laplacian_edge==0:
+                        self.B[frozenset(edge)] = -1
+                    else:
+                        self.B[frozenset(edge)] = 6 if (1/laplacian_edge)>6 else 1/laplacian_edge
 
                 if x+1 < frames.shape[1]:
                     edge = [(x, y,0), (x+1, y,0)]
                     laplacian_edge = (abs(axis1[edge[0][1], edge[0][0]]) + abs(axis1[edge[1][1], edge[1][0]]))/2
-                    self.B[frozenset(edge)] = -1 if laplacian_edge==0 else 1/laplacian_edge
+
+                    if laplacian_edge==0:
+                        self.B[frozenset(edge)] = -1
+                    else:
+                        self.B[frozenset(edge)] = 6 if (1/laplacian_edge)>6 else 1/laplacian_edge
 
 
         max_B = max(self.B.values())+1
@@ -107,9 +114,8 @@ class GraphAlgorithms:
             else:
                 hist, bin_edges = np.histogram(intensity, self.bin_edges)
                 idx = -np.where(hist==1)[0][0]
-                prob = 1e-6 if self.sHist[idx]==0 else self.sHist[idx]
+                prob = 0.01 if self.sHist[idx]==0 else self.sHist[idx]
                 R = -np.log(prob)*self.lmbda
-
                 return R
 
         if terminal == 'S':
@@ -120,7 +126,7 @@ class GraphAlgorithms:
             else:
                 hist, bin_edges = np.histogram(intensity, self.bin_edges)
                 idx = np.where(hist==1)[0][0]
-                prob = 1e-6 if self.tHist[idx]==0 else self.tHist[idx]
+                prob = 0.01 if self.tHist[idx]==0 else self.tHist[idx]
                 R = -np.log(prob)*self.lmbda
                 return R
 
@@ -147,6 +153,8 @@ class GraphAlgorithms:
         edges = []
         edge_colors = []
         weights = []
+        t_weights = []
+        n_weights = []
         for i in pixel_vertices:
             # t-links
             for terminal_vertex in terminal_vertices:
@@ -157,6 +165,7 @@ class GraphAlgorithms:
 
                 w = self.tLinkWeight(pixel, self.frames[pixel[2], pixel[1],pixel[0]], labels_dict[edge[1]])
                 weights.append(w)
+                t_weights.append(w)
 
             # n-links horizontally
             if (i%(imgw*imgh))%imgw < imgw - 1:
@@ -166,6 +175,7 @@ class GraphAlgorithms:
 
                 w = self.nLinkWeight(labels_dict[edge[0]], labels_dict[edge[1]])
                 weights.append(w)
+                n_weights.append(w)
 
             # n-links vertically
             if i%(imgw*imgh) < imgw * (imgh-1):
@@ -175,6 +185,7 @@ class GraphAlgorithms:
 
                 w = self.nLinkWeight(labels_dict[edge[0]], labels_dict[edge[1]])
                 weights.append(w)
+                n_weights.append(w)
 
             # n-links between frames
             if i+imgw*imgh < imgw * imgh * imgn:
@@ -184,6 +195,16 @@ class GraphAlgorithms:
 
                 w = self.nLinkWeight(labels_dict[edge[0]], labels_dict[edge[1]])
                 weights.append(w)
+                n_weights.append(w)
+
+        print("WEIGHTS")
+        print(np.mean(n_weights))
+        print(np.mean(t_weights))
+
+        plt.hist(n_weights,bins=40)
+        plt.show()
+        plt.hist(t_weights,bins=40)
+        plt.show()
 
         colors = ['#add8e6'] * (imgw * imgh * imgn) + ['#ffcccb', '#ffcccb']
 
